5b6fe32bb066202a9df869ff0c630d62bbfbf49f
Jota Teles <josetelesmaciel@gmail.com>
2016-07-09 14:14:58 -0300

    sem-issue - removendo html5 mode true

--- src/js/frontpress.config.js
+++ src/js/frontpress.config.js
@@ -6,8 +6,7 @@
     function frontpressConfig($interpolateProvider, $httpProvider, $urlRouterProvider, $locationProvider, BlogApiProvider, PostsApiProvider, $FrontpressProvider){
         $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-        $locationProvider.html5Mode(true);                    
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];                
         $FrontpressProvider.configure.load();
     }
 
