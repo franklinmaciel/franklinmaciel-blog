72191d1099c47b88207323be3bc0356fdc377337
Jota Teles <josetelesmaciel@gmail.com>
2016-07-09 14:36:29 -0300

    #14 - com spa hack 404

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@
     function frontpressConfig($interpolateProvider, $httpProvider, $urlRouterProvider, $locationProvider, BlogApiProvider, PostsApiProvider, $FrontpressProvider){
         $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+        $locationProvider.html5Mode(true);
         delete $httpProvider.defaults.headers.common['X-Requested-With'];                
         $FrontpressProvider.configure.load();
     }
